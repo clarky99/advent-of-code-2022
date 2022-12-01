e2495f66a21b757d99c461965631d53e508be336
Clarkonius Maximus <60491215+clarky99@users.noreply.github.com>
2022-12-01 17:04:43 -0500

    structural change

--- src/main/java/com/clarky99/advent2022/Solutions/Day1.java
+++ src/main/java/com/clarky99/advent2022/Solutions/Day1.java
@@ -1,39 +1,46 @@
 package com.clarky99.advent2022.Solutions;
 
 import com.clarky99.advent2022.Util.FileReader;
-
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day1 {
 
+    private static final String EOL = "\\r\\n";
+
     public static int solveOne() {
-        List<List<Integer>> inputs = readInput();
-        return inputs.stream()
-                .map(p -> p.stream().reduce(0, Integer::sum))
-                .mapToInt(p -> p)
-                .max()
-                .getAsInt();
+        List<Integer> elfs = readInput();
+
+        //return elfs.stream().reduce(Integer::max).get();
+        // We already have this in order so,
+        return elfs.get(0);
     }
 
     public static int solveTwo() {
-        List<List<Integer>> inputs = readInput();
-        List<Integer> integers = inputs.stream()
-                .map(p -> p.stream().reduce(0, Integer::sum))
-                .sorted(Comparator.reverseOrder())
-                .collect(Collectors.toList());
+        List<Integer> elfs = readInput();
 
-        return integers.get(0) + integers.get(1) + integers.get(2);
+        // return elfs.get(0) + elfs.get(1) + elfs.get(2);
+        // or use this cool function
+        return IntStream.range(0, 3).map(elfs::get).sum();
     }
 
-    private static List<List<Integer>> readInput() {
+    private static List<Integer> readInput() {
         String input = FileReader.readFileAsString("Day1.txt");
-        String[] items = input.split("\r\n\r\n");
 
-        return Arrays.stream(items)
-                .map(p -> Arrays.stream(p.split("\r\n")).map(Integer::valueOf).collect(Collectors.toList())).collect(Collectors.toList());
+        return Arrays.stream(input.split(EOL + EOL))
+                .map(elf -> Arrays.stream(elf.split(EOL)).mapToInt(Integer::parseInt).sum())
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        int maxCaloriesPerElf = Day1.solveOne();
+        int totalTopCalories = Day1.solveTwo();
+        System.out.println("Answer one:" + maxCaloriesPerElf);
+        System.out.println("Answer two:" + totalTopCalories);
     }
 
 }
